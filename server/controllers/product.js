5f9590378fd45aae6426983bd1dea530cf9011e3
bluesilver <bluesilver900702@gmail.com>
2019-04-21 00:13:45 +0700

    t5

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,16 +2,27 @@ var Product = require('../models/product');
 const ipInfo = require("ipinfo");
 var myip = require('quick-local-ip');
 const requestIp = require('request-ip');
+const ipdetails = require("node-ip-details");
+
+const http = require('http');
+const userIP = require('user-ip');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
     ipInfo((err, cLoc) => {
+        console.log("-------ipinfo");
         console.log(err || cLoc);
     });
+    console.log("-------myip");
     console.log(myip.getLocalIP4());
     // console.log(myip.getLocalIP6());
     const clientIp = requestIp.getClientIp(req); 
+    console.log("-------request-ip");
     console.log(clientIp);
+    
+    console.log("-------userIP");
+    console.log(userIP(req));
+
     res.send('Greetings from the Test controller!');
 };
 
