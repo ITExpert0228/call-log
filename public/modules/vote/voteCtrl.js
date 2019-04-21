949171c4e2db6d274455bff6ef5f420685e12895
bluesilver <bluesilver900702@gmail.com>
2019-04-21 11:21:56 +0700

    t9

--- public/modules/vote/voteCtrl.js
+++ public/modules/vote/voteCtrl.js
@@ -119,6 +119,62 @@ app.controller('VoteCtrl', ['$scope', '$rootScope', '$location', '$routeParams',
         });
     }
 
+    $scope.showStatistic = function($event, team) {
+        $event.stopPropagation();
+        $event.preventDefault();
+        if (team == 1) {
+            if ($('.team-b').hasClass('active')) {
+                $('.team-b').animate({
+                    width: "50%"
+                });
+                $('.team-b').removeClass('active');
+                $('.team-a').fadeIn('show');
+                $('.vote-status-bar').fadeIn();
+                $('.statistic-bar').fadeOut();
+                $('.team-details').fadeIn();
+                $('.vs-d').fadeIn();
+            } else {
+                $('.team-b').animate({
+                    width: "100%"
+                });
+                $('.team-b').addClass('active');
+                $('.team-a').fadeOut('slow');
+                $('.vote-status-bar').fadeOut();
+                $('.statistic-bar').fadeIn();
+                $('.team-details').fadeOut();
+                $('.vs-d').fadeOut();
+
+                $scope.showChat(team);
+            }
+            $scope.selectedMedia = $scope.optio.oRMedia;
+        } else {
+            if ($('.team-a').hasClass('active')) {
+                $('.team-a').animate({
+                    width: "50%"
+                });
+                $('.team-a').removeClass('active');
+                $('.team-b').fadeIn('show');
+                $('.vote-status-bar').fadeIn();
+                $('.statistic-bar').fadeOut();
+                $('.team-details').fadeIn();
+                $('.vs-d').fadeIn();
+            } else {
+                $('.team-a').animate({
+                    width: "100%"
+                });
+                $('.team-a').addClass('active');
+                $('.team-b').fadeOut('slow');
+                $('.vote-status-bar').fadeOut();
+                $('.statistic-bar').fadeIn();
+                $('.team-details').fadeOut();
+                $('.vs-d').fadeOut();
+
+                $scope.showChat(team);
+            }
+            $scope.selectedMedia = $scope.optio.oLMedia;
+        }
+    }
+
     $scope.voteSelect = function(team) {
         if (team == 1) {
             if ($('.team-b').hasClass('active')) {
@@ -129,6 +185,7 @@ app.controller('VoteCtrl', ['$scope', '$rootScope', '$location', '$routeParams',
                 $('.team-a').fadeIn('show');
                 $('.vote-status-bar').fadeIn();
                 $('.vote-bar').fadeOut();
+                $('.statistic-bar').fadeOut();
                 $('.team-details').fadeIn();
                 $('.vs-d').fadeIn();
             } else {
@@ -152,6 +209,7 @@ app.controller('VoteCtrl', ['$scope', '$rootScope', '$location', '$routeParams',
                 $('.team-b').fadeIn('show');
                 $('.vote-status-bar').fadeIn();
                 $('.vote-bar').fadeOut();
+                $('.statistic-bar').fadeOut();
                 $('.team-details').fadeIn();
                 $('.vs-d').fadeIn();
             } else {
@@ -166,6 +224,75 @@ app.controller('VoteCtrl', ['$scope', '$rootScope', '$location', '$routeParams',
                 $('.vs-d').fadeOut();
             }
             $scope.selectedMedia = $scope.optio.oLMedia;
+        }
+    }
+
+    $scope.showChat = function (team) {
+        var data1, data2, data3, data4;
+
+        google.charts.load("current", {packages:["corechart"]});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            if (team == 0) {
+                data1 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Yes',  $scope.percentage_yes],
+                    ['No',  100-$scope.percentage_yes]
+                    ]);
+                data2 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Love',  $scope.percentage_love],
+                    ['Hate',  100-$scope.percentage_love]
+                    ]);
+                data3 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Good',  $scope.percentage_good],
+                    ['Bad',  100-$scope.percentage_good]
+                    ]);
+                data4 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Smart',  $scope.percentage_smart],
+                    ['Dumb',  100-$scope.percentage_smart]
+                    ]);
+            } else {
+                data1 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['No',  $scope.percentage_yes],
+                    ['Yes',  100-$scope.percentage_yes]
+                    ]);
+                data2 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Hate',  $scope.percentage_love],
+                    ['Love',  100-$scope.percentage_love]
+                    ]);
+                data3 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Bad',  $scope.percentage_good],
+                    ['Good',  100-$scope.percentage_good]
+                    ]);
+                data4 = google.visualization.arrayToDataTable([
+                    ['Language', 'Speakers (in millions)'],
+                    ['Dumb',  $scope.percentage_smart],
+                    ['Smart',  100-$scope.percentage_smart]
+                    ]);
+            }
+            var options = {
+                legend: 'none',
+                pieSliceText: 'label',
+                backgroundColor: 'transparent'
+                // pieStartAngle: 100,
+            };
+
+            var chart1 = new google.visualization.PieChart(document.getElementById('chart1'));
+            chart1.draw(data1, options);
+            var chart2 = new google.visualization.PieChart(document.getElementById('chart2'));
+            chart2.draw(data2, options);
+            var chart3 = new google.visualization.PieChart(document.getElementById('chart3'));
+            chart3.draw(data3, options);
+            var chart4 = new google.visualization.PieChart(document.getElementById('chart4'));
+            chart4.draw(data4, options);
         }
     }
 
